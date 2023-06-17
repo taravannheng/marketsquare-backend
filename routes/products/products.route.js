93c3651928db6be34244a455405ae58ab6ef10cf
Taravann Heng <heng.t.taravann@gmail.com>
2023-06-18 03:12:08 +1000

    feat(route): add route for searching products
    close #39

--- routes/products/products.route.js
+++ routes/products/products.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productsSample = require('../../sample/products/product.sample.json');
-const { createProduct, getProduct, getMultipleProducts, getProducts, updateProduct, deleteProduct } = require('../../controllers/products/products.controller');
+const { createProduct, getProduct, getMultipleProducts, getProducts, searchProducts, updateProduct, deleteProduct } = require('../../controllers/products/products.controller');
 
 router.post('/products', createProduct);
 router.get('/products/batch', getMultipleProducts);
+router.get('/products/search', searchProducts);
 router.get('/products/:productID', getProduct);
 router.get('/products', getProducts);
 router.put('/products/:productID', updateProduct);
