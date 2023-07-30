db40d9b4f9dc88d8e6eaf4833dc67dd01a335bbc
Taravann Heng <heng.t.taravann@gmail.com>
2023-07-30 20:17:45 +1000

    refactor(config): add server url to oauth callbacks

--- configs/passport.js
+++ configs/passport.js
@@ -40,7 +40,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: `${process.env.LOCALHOST}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -78,7 +78,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "/api/auth/facebook/callback",
+      callbackURL: `${process.env.LOCALHOST}/api/auth/facebook/callback`,
       profileFields: ["id", "emails", "name", "photos"],
     },
     async (accessToken, refreshToken, profile, done) => {
