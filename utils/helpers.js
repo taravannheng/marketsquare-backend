9d6a755a07105f7ab6b83acdc502c9c8e334c1a7
Taravann Heng <heng.t.taravann@gmail.com>
2023-07-30 15:24:35 +1000

    feat(utils): add function to verify password

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,23 @@
-const argon2 = require('argon2');
+const argon2 = require("argon2");
 
 const generateCartID = async () => {
-  const nanoid = (await import('nanoid')).nanoid;
+  const nanoid = (await import("nanoid")).nanoid;
   const cartID = nanoid();
 
   return cartID;
 };
 
 const generateUserId = async () => {
-  const nanoid = (await import('nanoid')).nanoid;
+  const nanoid = (await import("nanoid")).nanoid;
   const userId = nanoid();
 
   return userId;
 };
 
 const generateOrderID = async () => {
-  const { customAlphabet } = await import('nanoid');
+  const { customAlphabet } = await import("nanoid");
 
-  const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const generate = customAlphabet(alphabet, 12);
   const orderID = generate();
 
@@ -25,33 +25,38 @@ const generateOrderID = async () => {
 };
 
 const getFirstThreeChars = (ids) => {
-  const result = ids.map(id => id.slice(0, 3)).join('');
+  const result = ids.map((id) => id.slice(0, 3)).join("");
   return result;
-}
+};
 
+// write a function called hashPassword which uses argon2 to hash a password
 const hashPassword = async (password) => {
-  let hashedPassword;
-  const hashedPasswordString = (async () => {
-    const hash = await argon2.hash(password, {
-      memoryCost: 1 << 19,
-      timeCost: 2,
-      parallelism: 1,
-      type: argon2.argon2id,
-    });    
+  // let hashedPassword;
+  const hashedPassword = (async () => {
+    const hash = await argon2.hash(password);
     return hash;
   })();
 
-  const regex = /^\$argon2id\$v=\d+\$m=\d+,t=\d+,p=\d+\$[A-Za-z0-9+\/]+\$([A-Za-z0-9+\/]+)$/;
-  const match = (await hashedPasswordString).toString().match(regex);
-
-  if (match && match[1]) {
-    hashedPassword = match[1];
-  } else {
-    hashedPassword = null;
-  }
-
   return hashedPassword;
-}
+};
 
+// write a function called verifyPassword which uses argon2 to verify a password
+const verifyPassword = async (hashedPassword, plainPassword) => {
+  try {
+    const isPasswordValid = await argon2.verify(hashedPassword, plainPassword);
 
-module.exports = { generateOrderID, generateCartID, getFirstThreeChars, hashPassword, generateUserId };
+    return isPasswordValid;
+  } catch (err) {
+    // internal failure
+    return false;
+  }
+};
+
+module.exports = {
+  generateOrderID,
+  generateCartID,
+  getFirstThreeChars,
+  hashPassword,
+  generateUserId,
+  verifyPassword,
+};
